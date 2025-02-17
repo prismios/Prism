a5569eb43bb622f897b5bc41393dea95a4812d8a
dull <adbrayford@icloud.com>
2025-02-16 21:01:32 -0800

    Initial Commit

---
+++ Prism/PrismApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrismApp.swift
+//  Prism
+//
+//  Created by Adam Brayford on 2025-02-16.
+//
+
+import SwiftUI
+
+@main
+struct PrismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
